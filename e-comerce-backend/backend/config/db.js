98910552c99dabe18765c879423ce6b44207b058
Ashutosh gupta <ashutosh.gupta.civ20@itbhu.ac.in>
2023-12-19 13:35:50 +0530

    my commit

---
+++ e-comerce-backend/backend/config/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URI || 'mongodb+srv://ashutoshtagupta:ashutosh@cluster0.e8mzket.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    )
+
+    console.log('MongoDB connection SUCCESS')
+  } catch (error) {
+    console.error('MongoDB connection FAIL')
+    process.exit(1)
+  }
+}
+
+module.exports = {connectDB}
